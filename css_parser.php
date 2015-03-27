c741b1674f26e5fe323c186041ca0f3e2d974649
quakig <bha100710@gmail.com>
2015-03-27 20:10:30 +0530

    css parser modified to handle media queries

--- css_parser.php
+++ css_parser.php
@@ -2,8 +2,12 @@
 
 function parse($css){
  
-    $css = preg_replace('/@import[^;]*;/', '', $css); // ignore all imports
     $css = preg_replace('~\/\*[^*]*\*+([^/*][^*]*\*+)*\/~', '', $css); // remove all multiline comments
+    $css = preg_replace('/@import[^;]*;/', '', $css); // ignore all imports
+    
+    $css = preg_replace('/@media[^{]*{/', '', $css); // ignore all media
+    $css = preg_replace('/}\s*}/', '}', $css); // remove double closing brackets
+    
     preg_match_all( '/([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)/', $css, $arr);
     $result = array();
     foreach ($arr[0] as $i => $x){
